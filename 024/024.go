9a4c42228a2a07c795181339d1dd1c59786a178e
Chavez <nailsbrokeandfell@gmail.com>
2014-04-19 13:36:58 -0700

    Comment and cleanup

--- 024/024.go
+++ 024/024.go
@@ -1,3 +1,12 @@
+// Problem 24
+// A permutation is an ordered arrangement of objects.
+// For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
+// If all of the permutations are listed numerically or alphabetically, we call
+// it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+//
+// 012   021   102   120   201   210
+//
+// What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 package main
 
 import (
@@ -22,15 +31,9 @@ func permutations(list []int, idx int) {
 		addPerm(list)
 	} else {
 		for i := idx; i < len(list); i++ {
-			tmp := list[idx]
-			list[idx] = list[i]
-			list[i] = tmp
-
+			list[idx], list[i] = list[i], list[idx]
 			permutations(list, idx+1)
-
-			tmp = list[idx]
-			list[idx] = list[i]
-			list[i] = tmp
+			list[idx], list[i] = list[i], list[idx]
 		}
 	}
 }
